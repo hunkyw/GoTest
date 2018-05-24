ca4d70cdf7ce98f57ac260e83121eca30d6c0939
Hunk Yuan <hunkyw@163.com>
2018-05-25 01:44:44 +0800

    Initial commit

---
+++ func.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func eval(a,b int, op string) int{
+	switch op {
+	case "+" :
+		return a + b
+	case "-" :
+		return a - b
+	case "*" :
+		return a * b
+	case "/" :
+		return a / b
+	default:
+		panic("unsupported operation : "+op)
+	}
+}
+
+func div (a,b int) (q ,r  int){
+	return a / b, a % b
+}
+func main() {
+	fmt.Println(eval(3,4 ,"*"))
+	fmt.Println(div(13,3))
+}
