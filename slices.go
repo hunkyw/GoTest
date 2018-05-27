bf4394256d3d83e9be40c9d8afcf21649f509ed1
Hunk Yuan <hunkyw@163.com>
2018-05-27 17:51:28 +0800

    Initial commit

--- slices.go
+++ slices.go
@@ -33,4 +33,13 @@ func main() {
 	fmt.Println(s2)
 	s2=s2[2:]
 	fmt.Println(s2)
+
+	fmt.Println("Extending slice")
+	arr1 := [...] int {0,1,2,3,4,5,6}
+	s3 := arr1[2:6]
+	s4 :=s3[3:5]
+	fmt.Printf("s3 = %v , len(s3) = %d , cap(s3) = %d \n",s3,len(s3),cap(s3))
+	fmt.Printf("s4 = %v , len(s4) = %d , cap(s4) = %d \n",s4,len(s4),cap(s4))
+	fmt.Println(s3)
+	fmt.Println(s4)
 }
