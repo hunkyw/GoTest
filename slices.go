0b4778b28716ef25fb045f1b7753f2e7585c4f53
Hunk Yuan <hunkyw@163.com>
2018-05-27 17:13:27 +0800

    Initial commit

---
+++ slices.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func updataSlice(s []int){
+	s[0]=100
+}
+
+func main() {
+	arr := [...]int{0,1,2,3,4,5,6,7}
+	fmt.Println("arr[2:6]=",arr[2:6])
+	fmt.Println("arr[:6]=",arr[:6])
+	s1 :=arr[2:]
+	fmt.Println("s1=",s1)
+	s2 := arr[:]
+	fmt.Println("s2=",s2)
+
+
+	fmt.Println("After updateSlice(s1)")
+	updataSlice(s1)
+	fmt.Println(s1)
+	fmt.Println(arr)
+	fmt.Println("After updateSlice(s2)")
+	updataSlice(s2)
+	fmt.Println(s2)
+	fmt.Println(arr)
+}
