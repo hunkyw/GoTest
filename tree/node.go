31b12a01a7935b048c13fb5c80dfbf8d17088f58
Hunk Yuan <hunkyw@163.com>
2018-05-28 01:43:58 +0800

    Initial commit

--- tree/node.go
+++ tree/node.go
@@ -2,28 +2,36 @@ package main
 
 import "fmt"
 
-type treenode struct {
+type treeNode struct {
 	value int
-	left,right *treenode
+	left,right *treeNode
 }
 
-func createNode(value int) *treenode {
-	return &treenode{value:value}
+func (node treeNode) print() {
+
+	fmt.Print(node.value)
 }
+
+func (node *treeNode) setValue (value int){
+	node.value = value
+}
+
+func createNode(value int) *treeNode {
+	return &treeNode{value:value}
+}
+
 
 func main() {
-	var  root  treenode
+	var  root  treeNode
 
-	root = treenode{value:3}
-	root.left = &treenode{}
-	root.right = &treenode{5,nil,nil}
-	root.right.left = new(treenode)
+	root = treeNode{value:3}
+	root.left = &treeNode{}
+	root.right = &treeNode{5,nil,nil}
+	root.right.left = new(treeNode)
 
-	root.right.left = createNode(2)
-	nodes := []treenode{
-		{value:3},
-		{},
-		{6,nil,&root},
-	}
-	fmt.Println(nodes)
+	root.left.right = createNode(2)
+
+	root.right.left.setValue(4)
+    root.right.left.print()
+    fmt.Println()
 }
