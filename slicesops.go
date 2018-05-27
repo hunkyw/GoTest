f00eb9bfc30586377e22e0b0a1ecc96f030e5815
Hunk Yuan <hunkyw@163.com>
2018-05-27 18:10:02 +0800

    Initial commit

--- slicesops.go
+++ slicesops.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func printSlices(s []int)  {
-	fmt.Printf("len = %d , cap = %d \n",len(s),cap(s))
+	fmt.Printf("%v,len = %d , cap = %d \n",
+		s,len(s),cap(s))
 }
 func main() {
 	var  s [] int  //Zero value for slice is nil
@@ -14,5 +17,15 @@ func main() {
 
 	}
 	fmt.Println(s)
+
+	s1 := [] int {2,4,6,8}
+	printSlices(s1)
+
+	s2 := make([]int , 16)
+
+	s3 := make([]int ,10 ,32)
+
+	printSlices(s2)
+	printSlices(s3)
 }
 
