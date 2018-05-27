f653a8efc3438701697ee79232435cdf770ff3aa
Hunk Yuan <hunkyw@163.com>
2018-05-27 18:48:48 +0800

    Initial commit

---
+++ maps.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string]string{
+		"name":"ccmouse",
+		"course":"golang",
+		"site":"imooc",
+		"quality":"notbad",
+	}
+    m2 := make(map[string]int) //m2 ==empty map
+
+	var  m3  map[string]int // m3 == nil
+	fmt.Println(m,m2,m3)
+}
