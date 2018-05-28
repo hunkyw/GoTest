84a1236c8dad05f796dc683fd8c73f8cdcdc1033
Hunk Yuan <hunkyw@163.com>
2018-05-28 16:44:00 +0800

    Initial commit

--- queue/queue.go
+++ queue/queue.go
@@ -1,5 +1,15 @@
-package main
+package queue
+type Queue []int
 
-func main() {
-	
+func (q *Queue) Push(v int){
+	*q = append(*q, v)
+
+}
+func (q *Queue) Pop() int{
+	head := (*q)[0]
+	*q = (*q)[1:]
+	return head
+}
+func (q *Queue) IsEmpty() bool{
+	return len(*q) == 0
 }
