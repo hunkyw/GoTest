cc6c45516708f849875db1e535be652792ec9985
Hunk Yuan <hunkyw@163.com>
2018-05-29 20:36:05 +0800

    Initial commit

--- retriever/main.go
+++ retriever/main.go
@@ -1,43 +1,85 @@
 package main
 
 import (
-	"retriever/mock"
 	"fmt"
-	"retriever/real"
+
 	"time"
+
+	"retriever/mock"
+
+	"retriever/real"
 )
 
 type Retriever interface {
 	Get(url string) string
 }
 
-func download(r Retriever)  string {
-	return r.Get("http://www.imooc.com")
+type Poster interface {
+	Post(url string,
+		form map[string]string) string
+}
+
+const url = "http://www.imooc.com"
+
+func download(r Retriever) string {
+	return r.Get(url)
+}
+
+func post(poster Poster) {
+	poster.Post(url,
+		map[string]string{
+			"name":   "ccmouse",
+			"course": "golang",
+		})
+}
+
+type RetrieverPoster interface {
+	Retriever
+	Poster
+}
+
+func session(s RetrieverPoster) string {
+	s.Post(url, map[string]string{
+		"contents": "another faked imooc.com",
+	})
+	return s.Get(url)
 }
 
 func main() {
-	var r  Retriever
-	r = mock.Retriever{"this is a fake imooc.com"}
+	var r Retriever
+
+	mockRetriever := mock.Retriever{
+		Contents: "this is a fake imooc.com"}
+	r = &mockRetriever
 	inspect(r)
-    r = &real.Retriever{
-    	UserAgent:"Mozilla/5.0",
-    	TimeOut:time.Minute,
+
+	r = &real.Retriever{
+		UserAgent: "Mozilla/5.0",
+		TimeOut:   time.Minute,
+	}
+	inspect(r)
+
+	// Type assertion
+	if mockRetriever, ok := r.(*mock.Retriever); ok {
+		fmt.Println(mockRetriever.Contents)
+	} else {
+		fmt.Println("r is not a mock retriever")
 	}
 
-	inspect(r)
-
-    //type assertion
-    realRetriever := r.(*real.Retriever)
-    fmt.Println(realRetriever.TimeOut)
-	//fmt.Println(download(r))
+	fmt.Println(
+		"Try a session with mockRetriever")
+	fmt.Println(session(&mockRetriever))
 }
 
 func inspect(r Retriever) {
-	fmt.Printf("%T %v\n",r,r)
+	fmt.Println("Inspecting", r)
+	fmt.Printf(" > Type:%T Value:%v\n", r, r)
+	fmt.Print(" > Type switch: ")
 	switch v := r.(type) {
-	case mock.Retriever:
-		fmt.Println("Conrents:", v.Contents)
+	case *mock.Retriever:
+		fmt.Println("Contents:", v.Contents)
 	case *real.Retriever:
-		fmt.Println("UseAgent", v.UserAgent)
+		fmt.Println("UserAgent:", v.UserAgent)
 	}
+	fmt.Println()
 }
--- retriever/real/retriever.go
+++ retriever/real/retriever.go
@@ -1,28 +1,28 @@
 package real
 
 import (
-	"time"
 	"net/http"
 	"net/http/httputil"
+	"time"
 )
 
 type Retriever struct {
 	UserAgent string
-	TimeOut time.Duration
+	TimeOut   time.Duration
 }
 
-func (r *Retriever) Get(url string) string{
-    resp,err := http.Get(url)
+func (r *Retriever) Get(url string) string {
+	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
 
-	result,err := httputil.DumpResponse(
+	result, err := httputil.DumpResponse(
 		resp, true)
 
 	resp.Body.Close()
 
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 
