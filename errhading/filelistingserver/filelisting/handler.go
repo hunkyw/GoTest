f92c7fc8595e8137ee27581f72f05c5495fab350
Hunk Yuan <hunkyw@163.com>
2018-05-31 01:32:12 +0800

    Initial commit

--- errhading/filelistingserver/filelisting/handler.go
+++ errhading/filelistingserver/filelisting/handler.go
@@ -4,10 +4,18 @@ import (
 	"net/http"
 	"os"
 	"io/ioutil"
+	"strings"
+	"errors"
 )
 
+const prefix = "/"
+
 func HandleFileList (writer http.ResponseWriter, request *http.Request)error {
-	path := request.URL.Path[len("/"):]
+	if strings.Index(request.URL.Path,prefix) != 0{
+		return errors.New("path must start " + "with" + prefix )
+	}
+
+	path := request.URL.Path[len(prefix):]
 	file, err := os.Open(path)
 	if err != nil {
 		return err
