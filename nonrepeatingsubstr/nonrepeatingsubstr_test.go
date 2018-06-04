7eb685b9f09aab02d1ea1fceb4def13372044527
Hunk Yuan <hunkyw@163.com>
2018-06-04 15:03:43 +0800

    Initial commit

--- nonrepeatingsubstr/nonrepeatingsubstr_test.go
+++ nonrepeatingsubstr/nonrepeatingsubstr_test.go
@@ -33,7 +33,13 @@ func TestSubstr(t *testing.T){
 
 func BenchmarkSubstr(b *testing.B)  {
 	s := "黑化肥会挥发发灰会花飞灰化肥挥发发黑会飞花"
+	for i := 0 ; i < 13 ; i++ {
+		s = s + s
+	}
+	b.Logf("len(s) = %d ",len(s))
 	ans := 8
+
+	b.ResetTimer()
 
 	for i := 0 ; i < b.N ; i++ {
 		actual := lengthOfNonrepeatingSubStr(s)
