3f34e86935e49e24b3c594f9f3be363b8f25d510
Hunk Yuan <hunkyw@163.com>
2018-06-05 14:07:23 +0800

    Initial commit

--- gooutine/gooutine.go
+++ gooutine/gooutine.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"time"
+	"fmt"
+	"runtime"
 )
 
 func main() {
-	for i := 0 ; i < 1000 ; i++{
+	var a  [10]int
+	for i := 0 ; i < 10 ; i++{
 		go func(i int) {
 			for  {
-				fmt.Printf("Hello from " + "groutine %d \n", i)
+				a[i]++
+				runtime.Gosched()
 			}
 		}(i)
 	}
 	time.Sleep(time.Millisecond)
+	fmt.Println(a)
 }
